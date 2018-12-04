844b3cff4c67272958066cae191bcc2c31c9b530
Szakalakazo <tomek.starzak@gmail.com>
2018-12-04 10:13:04 +0100

    MenuChoice comments added

--- src/main/java/appNavigation/menuChoice/MenuChoice.java
+++ src/main/java/appNavigation/menuChoice/MenuChoice.java
@@ -3,6 +3,14 @@ package appNavigation.menuChoice;
 import java.io.IOException;
 import java.util.Scanner;
 
+//Klasa pobiera dane poprzez klasę UserInput i przyjmuję trzy argumrnty:
+//regex, specialChar i specialCharAction. Regex odpowiada za wybór opcji (np 1-9)
+//specialChar za akcje specjalne (np 'q' jak quit)
+//specialCharAction za akcję powiązaną ze specialChar
+//Pętla wykonuję przynajmniej jeden "obrót" i działa, dopuki warunek isValid jest false
+//Warunek jest definiownany w metodzie isValid, która pobiera getUserinput, regex, specialChar, porównuje je i zwraca true lub false
+
+
 public class MenuChoice {
 
     public static String validateInput(String regex, String specialChar, String specialCharAction) {
