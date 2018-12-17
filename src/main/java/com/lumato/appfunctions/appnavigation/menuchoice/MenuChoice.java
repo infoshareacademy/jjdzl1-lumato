0a864a61fc6d2cfff4517eaa517852fbdd6880ea
Szakalakazo <tomek.starzak@gmail.com>
2018-12-17 20:00:17 +0100

    WriteRadFile uses user path and file name

--- src/main/java/com/lumato/appfunctions/appnavigation/menuchoice/MenuChoice.java
+++ src/main/java/com/lumato/appfunctions/appnavigation/menuchoice/MenuChoice.java
@@ -1,6 +1,9 @@
 package com.lumato.appfunctions.appnavigation.menuchoice;
 
 import com.lumato.appfunctions.carsmanagement.UserCarsPanel;
+import com.lumato.inout.FilePaths;
+import com.lumato.inout.UserInput;
+import com.lumato.inout.WriteReadFile;
 import com.lumato.tools.CLS;
 
 import java.io.IOException;
@@ -14,10 +17,12 @@ public class MenuChoice {
                 UserCarsPanel.init();
                 break;
             case '2':
-                System.out.println("Opcja druga.");
+                System.out.println("Opcja druga. Zapis do pliku");
+                WriteReadFile.writeText(UserInput.getUserStringInput(),true,"Test.csv");
                 break;
             case '3':
-                System.out.println("Opcja trzecia.");
+                System.out.println("Opcja trzecia. Odczyty z pliku");
+                WriteReadFile.readCSV(1,"Test.csv");
                 break;
             case 'q':
                 System.out.println("Zakończenie programu lub coś innego.");
